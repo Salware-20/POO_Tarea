975fc0f21b2765ab45503eb01187c4df89ae4641
Andrés <maringusman11@gmail.com>
2025-09-25 23:36:17 -0600

    Ejercicio 2 terminado
    Fue sobre herencia y sobrescritura
    sinceramente es muy dificil hacerlo igual que como hice el primero

--- Ejercicios/Ejercicio#1.cs
+++ Ejercicios/Ejercicio#1.cs
@@ -8,54 +8,57 @@ public class CuentaBancaria
     public void DepositarDinero(double monto)
     {
         if (monto > 0) { _saldo += monto; Console.WriteLine("Deposito exitoso!"); }
-        
+        //Esta funcion de aca se encarga de verificar que el monto no sea 0 y si es asi entonces realiza el deposito
     }
 
     public void RetirarDinero(double monto)
     {
         if (monto <= _saldo) { _saldo -= monto; Console.WriteLine("Retiro exitoso!"); }
         else { Console.WriteLine($"Lo sentimos\nSaldo actual: {_saldo}"); }
+        // Lo mismo que depositar pero esta vez con el retiro de verificando que el usuario no retire más de lo que tiene en su cuenta 
     }
 
     public double VerSaldo()
     {
         return _saldo;
+        // muetras el dinero que tiene y ya
     }
 }
 
-public class Program
-{
-    static void Main()
-    {
-        CuentaBancaria Cuenta = new CuentaBancaria();
-        while (true)
-        {
-            try
-            {
-                Console.WriteLine("Que deseas hacer: \n1] Depositar\n2] Retirar\n3] Ver saldo\n4] Cerrar");
-                string? input = Console.ReadLine();
-                int opcion = Convert.ToInt32(input);
-                if (opcion == 1)
-                {
-                    Console.Write("Cuanto deseas depositar: ");
-                    string? input_ = Console.ReadLine();
-                    double monto = Convert.ToDouble(input_);
-                    Cuenta.DepositarDinero(monto);
-                }
-                else if (opcion == 2) 
-                {
-                    Console.Write("Cuanto deseas retirar: ");
-                    string? input_ = Console.ReadLine();
-                    double monto = Convert.ToDouble(input_);
-                    Cuenta.RetirarDinero(monto);
-                }
-                else if (opcion == 3)
-                {
-                    Console.WriteLine(Cuenta.VerSaldo());
-                }
-                else { Console.WriteLine("Nos vemos..."); break; }
-            }
-            catch (Exception ex) { Console.WriteLine($"ERROR: {ex.Message}"); }
-        }
-    }
-}
+//public class Program
+//{
+//    static void Main()
+//    {
+//        CuentaBancaria Cuenta = new CuentaBancaria();
+//        // Cree un menu donde usando while logro que el sistema no se caiga aun con errores y diferentes problemas que pueda presentar
+//        while (true)
+//        {
+//            try
+//            {
+//                Console.WriteLine("Que deseas hacer: \n1] Depositar\n2] Retirar\n3] Ver saldo\n4] Cerrar");
+//                string? input = Console.ReadLine();
+//                int opcion = Convert.ToInt32(input);
+//                if (opcion == 1)
+//                {
+//                    Console.Write("Cuanto deseas depositar: ");
+//                    string? input_ = Console.ReadLine();
+//                    double monto = Convert.ToDouble(input_);
+//                    Cuenta.DepositarDinero(monto);
+//                }
+//                else if (opcion == 2) 
+//                {
+//                    Console.Write("Cuanto deseas retirar: ");
+//                    string? input_ = Console.ReadLine();
+//                    double monto = Convert.ToDouble(input_);
+//                    Cuenta.RetirarDinero(monto);
+//                }
+//                else if (opcion == 3)
+//                {
+//                    Console.WriteLine(Cuenta.VerSaldo());
+//                }
+//                else { Console.WriteLine("Nos vemos..."); break; }
+//            }
+//            catch (Exception ex) { Console.WriteLine($"ERROR: {ex.Message}"); }
+//        }
+//    }
+//}
--- Ejercicios/Ejercicio#2.cs
+++ Ejercicios/Ejercicio#2.cs
@@ -1,1 +1,54 @@
-﻿
+﻿//--> Ejercicio 2 – Herencia y sobrescritura <--
+
+public class Empleado
+{
+    // Se crea la clase madre que es el empleado
+    public string? _nombre = "Andres";
+    public double _salario = 500;
+
+    public virtual void MostrarInformacion()
+    {
+        Console.WriteLine($"Nombre: {_nombre}");
+        Console.WriteLine($"Salario: {_salario}");
+    }
+}
+
+public class Gerente : Empleado
+{
+    // Con esta forma de poner geren : emple le estamos diciendo que gerente es hija de empleado
+    private int _bonificacion = 500;
+
+    public override void MostrarInformacion()
+    {
+        Console.WriteLine($"Nombre: {_nombre}");
+        Console.WriteLine($"Salario: {_salario}");
+        Console.WriteLine($"Bonificacion: {_bonificacion}");
+    }
+}
+
+public class Programador : Empleado
+{
+    private string _lenguaje = "C#";
+
+    public override void MostrarInformacion()
+    {
+        Console.WriteLine($"Nombre: {_nombre}");
+        Console.WriteLine($"Salario: {_salario}");
+        Console.WriteLine($"Lenguaje: {_lenguaje}");
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        // Imprimimos todo los datos de los usuarios
+        Empleado empleado1 = new Empleado();
+        empleado1.MostrarInformacion();
+        Gerente gerente1 = new Gerente();
+        gerente1.MostrarInformacion();
+        Programador programador1 = new Programador();
+        programador1.MostrarInformacion();
+        Console.ReadLine();
+    }
+}
